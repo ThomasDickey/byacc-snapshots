96df58092022a4abeb061e56d21ebda088499bcd
Thomas E. Dickey <dickey@invisible-island.net>
2014-03-22 01:18:13 -0400

    snapshot of project "byacc", label t20140321-gcov

---
+++ test/err_syntax4.tab.c
@@ -0,0 +1,13 @@
+#ifndef lint
+static const char yysccsid[] = "@(#)yaccpar	1.9 (Berkeley) 02/21/93";
+#endif
+
+#define YYBYACC 1
+#define YYMAJOR 1
+#define YYMINOR 9
+
+#define YYEMPTY        (-1)
+#define yyclearin      (yychar = YYEMPTY)
+#define yyerrok        (yyerrflag = 0)
+#define YYRECOVERING() (yyerrflag != 0)
+
